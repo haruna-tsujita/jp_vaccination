303fb8f1a02f28e9c37f70cdefdbb344f25940d9
napple29 <snwxxx29@gmail.com>
2022-03-07 14:55:09 +0900

    次の予防接種予定日を割り出すメソッドを実装

--- lib/jp_vaccination.rb
+++ lib/jp_vaccination.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'date'
 require 'json'
 require_relative './jp_vaccination/vaccination'
 require_relative './jp_vaccination/version'
@@ -11,8 +12,40 @@ module JpVaccination
   end
 
   def self.find(vaccination)
-    data = self.json_data
+    data = json_data
     vaccination_data = data[vaccination.to_sym]
     JpVaccination::Vaccination.new(vaccination_data)
+  end
+
+  def self.next_day(vaccination_name:, previous_day:)
+    next_day = {}
+    json_data.each do |key, vaccination|
+      next unless key == vaccination_name
+
+      name = "#{vaccination[:name]} #{vaccination[:period]}"
+
+      date_type = case vaccination[:interval]
+                  when nil
+                    calc_date(period: vaccination[:deadline], start_or_end: :start, date_type: previous_day)
+                  else
+                    calc_date(period: vaccination[:interval], start_or_end: :start, date_type: previous_day)
+                  end
+      next_day[:name] = name
+      next_day[:date_type] = date_type
+    end
+    next_day
+  end
+
+  def self.calc_date(period:, start_or_end:, date_type:)
+    case period[:date_type]
+    when 'day'
+      Date.parse(date_type) + period[start_or_end].to_i
+    when 'week'
+      Date.parse(date_type) + (period[start_or_end].to_i * 7)
+    when 'month'
+      Date.parse(date_type) >> period[start_or_end].to_i
+    when 'year'
+      Date.parse(date_type) >> (period[start_or_end].to_i * 12)
+    end
   end
 end
