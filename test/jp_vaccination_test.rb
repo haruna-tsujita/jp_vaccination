37c6afc5c8a44d000e6d0e42faadf9750d734413
napple29 <snwxxx29@gmail.com>
2022-03-07 16:12:39 +0900

    次の接種予定日の計算メソッドと日付の計算メソッドのテストを追加

--- test/jp_vaccination_test.rb
+++ test/jp_vaccination_test.rb
@@ -1,13 +1,62 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require_relative './test_helper'
+require_relative '../lib/jp_vaccination'
 
 class JpVaccinationTest < Minitest::Test
-  # def test_that_it_has_a_version_number
-  #   refute_nil ::JpVaccination::VERSION
-  # end
+  def test_next_day_when_interval_nil
+    vaccination = :hib_1
+    birthday = '2020-01-01'
+    next_day = JpVaccination.next_day(vaccination_name: vaccination, previous_day: birthday)
+    assert_equal next_day[:name], 'ヒブ １回目'
+    assert_equal next_day[:date_type].year, 2020
+    assert_equal next_day[:date_type].month, 3
+    assert_equal next_day[:date_type].day, 1
+  end
 
-  # def test_it_does_something_useful
-  #   assert false
-  # end
+  def test_next_day_when_interval
+    vaccination = :hepatitis_B_2
+    previous_day = '2020-01-01'
+    next_day = JpVaccination.next_day(vaccination_name: vaccination, previous_day: previous_day)
+    assert_equal next_day[:name], 'Ｂ型肝炎 ２回目'
+    assert_equal next_day[:date_type].year, 2020
+    assert_equal next_day[:date_type].month, 1
+    assert_equal next_day[:date_type].day, 28
+  end
+
+  def test_calc_date_when_day
+    vaccination_period = JpVaccination.json_data[:pneumococcus_2][:interval]
+    previous_day = '2020-01-01'
+    calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: previous_day)
+    assert_equal calc_date.year, 2020
+    assert_equal calc_date.month, 1
+    assert_equal calc_date.day, 28
+  end
+
+  def test_calc_date_when_week
+    vaccination_period = JpVaccination.json_data[:rotavirus_2][:interval]
+    previous_day = '2020-01-01'
+    calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: previous_day)
+    assert_equal calc_date.year, 2020
+    assert_equal calc_date.month, 1
+    assert_equal calc_date.day, 29
+  end
+
+  def test_calc_date_when_month
+    vaccination_period = JpVaccination.json_data[:Japanese_encephalitis_3][:interval]
+    previous_day = '2020-01-01'
+    calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: previous_day)
+    assert_equal calc_date.year, 2020
+    assert_equal calc_date.month, 7
+    assert_equal calc_date.day, 1
+  end
+
+  def test_calc_date_when_year
+    vaccination_period = JpVaccination.json_data[:MR_1][:deadline]
+    birthday = '2020-01-01'
+    calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: birthday)
+    assert_equal calc_date.year, 2021
+    assert_equal calc_date.month, 1
+    assert_equal calc_date.day, 1
+  end
 end
