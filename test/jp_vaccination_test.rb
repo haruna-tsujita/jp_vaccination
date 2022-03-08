057802de3bee8742e9c38c81a61f99ea7e0aaf57
napple29 <snwxxx29@gmail.com>
2022-03-08 11:08:38 +0900

    日付のテストコードが冗長だったためリファクタリング

--- test/jp_vaccination_test.rb
+++ test/jp_vaccination_test.rb
@@ -9,9 +9,7 @@ class JpVaccinationTest < Minitest::Test
     birthday = '2020-01-01'
     next_day = JpVaccination.next_day(vaccination_name: vaccination, previous_day: birthday)
     assert_equal next_day[:name], 'ヒブ １回目'
-    assert_equal next_day[:date_type].year, 2020
-    assert_equal next_day[:date_type].month, 3
-    assert_equal next_day[:date_type].day, 1
+    assert_equal next_day[:date_type], Date.parse('2020-03-01')
   end
 
   def test_next_day_when_interval
@@ -19,45 +17,35 @@ class JpVaccinationTest < Minitest::Test
     previous_day = '2020-01-01'
     next_day = JpVaccination.next_day(vaccination_name: vaccination, previous_day: previous_day)
     assert_equal next_day[:name], 'Ｂ型肝炎 ２回目'
-    assert_equal next_day[:date_type].year, 2020
-    assert_equal next_day[:date_type].month, 1
-    assert_equal next_day[:date_type].day, 28
+    assert_equal next_day[:date_type], Date.parse('2020-01-28')
   end
 
   def test_calc_date_when_day
     vaccination_period = JpVaccination.json_data['pneumococcus_2'.to_sym][:interval]
     previous_day = '2020-01-01'
     calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: previous_day)
-    assert_equal calc_date.year, 2020
-    assert_equal calc_date.month, 1
-    assert_equal calc_date.day, 28
+    assert_equal calc_date, Date.parse('2020-01-28')
   end
 
   def test_calc_date_when_week
     vaccination_period = JpVaccination.json_data['rotavirus_2'.to_sym][:interval]
     previous_day = '2020-01-01'
     calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: previous_day)
-    assert_equal calc_date.year, 2020
-    assert_equal calc_date.month, 1
-    assert_equal calc_date.day, 29
+    assert_equal calc_date, Date.parse('2020-01-29')
   end
 
   def test_calc_date_when_month
     vaccination_period = JpVaccination.json_data['Japanese_encephalitis_3'.to_sym][:interval]
     previous_day = '2020-01-01'
     calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: previous_day)
-    assert_equal calc_date.year, 2020
-    assert_equal calc_date.month, 7
-    assert_equal calc_date.day, 1
+    assert_equal calc_date, Date.parse('2020-07-01')
   end
 
   def test_calc_date_when_year
     vaccination_period = JpVaccination.json_data['MR_1'.to_sym][:deadline]
     birthday = '2020-01-01'
     calc_date = JpVaccination.calc_date(period: vaccination_period, start_or_end: :start, date_type: birthday)
-    assert_equal calc_date.year, 2021
-    assert_equal calc_date.month, 1
-    assert_equal calc_date.day, 1
+    assert_equal calc_date, Date.parse('2021-01-01')
   end
 
   def test_recommended_schedules # rubocop:disable Metrics/MethodLength
